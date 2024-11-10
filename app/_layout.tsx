e11125e081923836ba110dee39c5c8de03e70bfb
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-11-10 13:20:20 +0000

    config(router): Implementación de componentes y estructura de directios de la librería Expo Router.

---
+++ app/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+
+const StackLayout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name="(tabs)"/>
+    </Stack>
+  )
+}
+
+export default StackLayout
---
+++ app/porfolio/(tabs)/samplePageB.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const samplePage = () => {
+  return (
+    <View>
+        <Text>Página 1 dentro de Tabs</Text>
+    </View>
+  )
+}
+
+export default samplePage
