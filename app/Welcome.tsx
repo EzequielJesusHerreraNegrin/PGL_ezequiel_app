9d12d74ccc48c0a647891223fa2d19be048fa88f
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-11-10 19:22:45 +0000

    build: add temporal elments to ubicate the final ones.

--- app/Welcome.tsx
+++ app/Welcome.tsx
@@ -1,19 +1,42 @@
-import { Link, Redirect } from 'expo-router'
+import { Link } from 'expo-router'
 import React from 'react'
-import { Text, View, StyleSheet, Pressable } from 'react-native'
+import { Text, View, StyleSheet, Pressable, Image } from 'react-native'
 
 function Welcome() {
   return (
-    <View>
-      <Text>Este el welcome</Text>
-      <Link href="/samplePage">AQUI</Link>
+    <View style={styleSheet.container}>
+      <View style={styleSheet.headerBox}>
+        <Text>Este el welcome</Text>
+      </View>
+      <View style={styleSheet.bodyBox}>
+        <View>
+          <Image src='assets\MySelf.jpeg'/>
+        </View>
+        <Link href="/samplePage">AQUI</Link>
+      </View>
     </View>
   )
 }
 
 const styleSheet = StyleSheet. create({
-  linkText: {
-    color: "black"
+  container:{
+    display : "flex",
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: "100%",
+    width: "100%",
+    backgroundColor: "red",
+  },
+  headerBox:{
+    backgroundColor: "blue",
+    flex: 2
+  },
+  bodyBox: {
+    backgroundColor: "green",
+    flex: 5
+  },
+  imageStyle:{
+    
   }
 })
 
