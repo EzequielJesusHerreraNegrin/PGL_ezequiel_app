2e21aa1325dadc9ca9797ab644dd46491c21b584
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-11-12 18:17:16 +0000

    feat: Implement a recicled component, MyCard, component of other project as the new Header of the porfolio section.

--- app/porfolio/(tabs)/samplePageB.tsx
+++ app/porfolio/(tabs)/samplePageB.tsx
@@ -1,13 +1,12 @@
-
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
 const samplePage = () => {
   return (
     <View>
-        <Text>Página 1 dentro de Tabs</Text>
+      <Text>Página 2 dentro de Tabs</Text>
     </View>
-  )
-}
+  );
+};
 
-export default samplePage
+export default samplePage;
