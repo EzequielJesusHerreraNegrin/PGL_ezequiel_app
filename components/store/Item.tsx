2159033029fb3b58ce9bd67bdee1b18727783c37
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-11-27 01:21:48 +0000

    feat(Ttem): temporal item styles

--- components/store/Item.tsx
+++ components/store/Item.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+import { StyleSheet, Text, View, Image, Button, Pressable } from "react-native";
 import { foodItem } from "../../types/AppTypes";
 import React from "react";
 
@@ -27,12 +27,22 @@ const Item = (props: FoodItemProps) => {
   };
 
   return (
-    <View>
-      <View>
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
         <Image source={itemImage()} style={styles.image} />
+      </View>
+      <View style={styles.container}>
         <Text>Nombre: {props.foodItem.name}</Text>
         <Text>Categoría: {props.foodItem.section}</Text>
         <Text>Precio: {props.foodItem.price}€</Text>
+      </View>
+      <View style={[styles.container, styles.container]}>
+        <Pressable style={styles.buttom}>
+          <Text style={styles.buttonText}>Basket</Text>
+        </Pressable>
+        <Pressable style={styles.buttom}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -41,8 +51,35 @@ const Item = (props: FoodItemProps) => {
 export default Item;
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    display: "flex",
+    flexDirection: "row",
+    borderRadius: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 5,
+    margin: 5,
+  },
+  container: {
+    width: "30%",
+    gap: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    margin: "auto",
+    left: "5%",
+  },
   image: {
     width: 100,
     height: 100,
+  },
+  buttom: {
+    width: 50,
+    height: 50,
+    backgroundColor: "yellow",
+    borderRadius: 10,
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
