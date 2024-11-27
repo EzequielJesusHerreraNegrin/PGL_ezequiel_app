d9bca9b29b1587e47ceec6c77b5642b9a7f9fcdc
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-11-27 00:38:01 +0000

    feat(Item): implementation of props and way to display images dinamicaly.

--- components/store/Item.tsx
+++ components/store/Item.tsx
@@ -1,12 +1,38 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 import { foodItem } from "../../types/AppTypes";
 import React from "react";
 
-const Item = (sectionListItem: foodItem) => {
+type FoodItemProps = {
+  foodItem: foodItem;
+};
+
+const Item = (props: FoodItemProps) => {
+  const itemImage = () => {
+    switch (props.foodItem.image) {
+      case "1":
+        return require("../../assets/storeImages/41.png");
+      case "2":
+        return require("../../assets/storeImages/42.png");
+      case "3":
+        return require("../../assets/storeImages/43.png");
+      case "4":
+        return require("../../assets/storeImages/41.png");
+      case "5":
+        return require("../../assets/storeImages/41.png");
+      case "6":
+        return require("../../assets/storeImages/41.png");
+      case "7":
+        return require("../../assets/storeImages/41.png");
+    }
+  };
+
   return (
     <View>
       <View>
-        <Image source={require(`${sectionListItem.image}+.png`)} />
+        <Image source={itemImage()} style={styles.image} />
+        <Text>Nombre: {props.foodItem.name}</Text>
+        <Text>Categoría: {props.foodItem.section}</Text>
+        <Text>Precio: {props.foodItem.price}€</Text>
       </View>
     </View>
   );
@@ -14,4 +40,9 @@ const Item = (sectionListItem: foodItem) => {
 
 export default Item;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+  },
+});
