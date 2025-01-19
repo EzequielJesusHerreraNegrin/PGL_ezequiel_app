1bf093378f6060bd8d44f8bf8e2bbe0397de156a
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2025-01-19 10:42:44 +0000

    feat: deleting commented text.

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -6,21 +6,21 @@ export const AppLayout = () => {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer>
         <Drawer.Screen
-          name="welcome/index" // This is the name of the page and must match the url from root
+          name="welcome/index"
           options={{
             drawerLabel: "Home",
             title: "Bienbenido",
           }}
         />
         <Drawer.Screen
-          name="porfolio" // This is the name of the page and must match the url from root
+          name="porfolio"
           options={{
             drawerLabel: "Porfolio",
             title: "Porfolio",
           }}
         />
         <Drawer.Screen
-          name="store/index" // This is the name of the page and must match the url from root
+          name="store/index"
           options={{
             drawerLabel: "Mi compra",
             title: "Mi compra",
